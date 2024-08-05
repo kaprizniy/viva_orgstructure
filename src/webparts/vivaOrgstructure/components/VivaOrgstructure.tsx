4517804d4a3b814ca5a147765051ebcdd27e00a1
Oleksandr Paniuta <o.paniuta@steel.mil.ua>
2024-08-05 11:43:52 +0300

    poor

--- src/webparts/vivaOrgstructure/components/VivaOrgstructure.tsx
+++ src/webparts/vivaOrgstructure/components/VivaOrgstructure.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { SPFI } from '@pnp/sp';
 import { getSP } from '../pnpjsConfig';
 import styles from './VivaOrgstructure.module.scss';
+import classNames from 'classnames';
 import type { IVivaOrgstructureProps, IVivaOrgstructureState } from './IVivaOrgstructure';
-
 export default class VivaOrgstructure extends React.Component<IVivaOrgstructureProps, IVivaOrgstructureState> {
 
   private _sp: SPFI;
@@ -28,7 +28,7 @@ export default class VivaOrgstructure extends React.Component<IVivaOrgstructureP
     try {
       const userItem: any = await this._sp.web
         .getList(this.props.context.pageContext.web.serverRelativeUrl + this.props.employeeListLink)
-        .items.select('OrgStructureJSON, FullName, AccountName')
+        .items.select('OrgStructureJSON, FullName, AccountName, Position')
         .filter(`AccountName eq '${this.state.loginName}'`)
         .top(1)();
       console.log('userItem', userItem);
@@ -40,7 +40,7 @@ export default class VivaOrgstructure extends React.Component<IVivaOrgstructureP
         //Get employee info
         var employeeItem: any = await this._sp.web
           .getList(this.props.context.pageContext.web.serverRelativeUrl + this.props.employeeListLink)
-          .items.select('OrgStructureJSON, FullName, AccountName')
+          .items.select('OrgStructureJSON, FullName, AccountName, Position')
           .filter(`ManagerAccount eq '${userItemJSON.Manager.Login}'`)
           .top(1000)();
         console.log('employeeItem', employeeItem);
@@ -53,13 +53,11 @@ export default class VivaOrgstructure extends React.Component<IVivaOrgstructureP
 
   private onClickItem(e: any) {
     console.log('EventID', e.target.id);
-    this.setState({ loginName: e.target.id });
-    this.getOrgStructure();
+    this.setState({ loginName: e.target.id }, () => { this.getOrgStructure() });
   }
 
 
   private requestPhotoUrl(userLogin: string) {
-    console.log('this.props.context.pageContext', this.props.context.pageContext);
     return `${this.props.context.pageContext.web.absoluteUrl}/_layouts/15/userphoto.aspx?UserName=${userLogin}@${this.props.loginDomain}&size=S`;
   }
 
@@ -74,7 +72,7 @@ export default class VivaOrgstructure extends React.Component<IVivaOrgstructureP
                 <div className={styles.userPhoto}>
                   <img src={this.requestPhotoUrl(userItem.Manager.Login)} id={userItem.Manager.Login} draggable="false" />
                 </div>
-                <div className={styles.userInfo} id={userItem.Manager.Login}>{userItem.Manager.Name} {userItem.Manager.FamilyName}</div>
+                <div className={styles.userInfo} id={userItem.Manager.Login}><p>{userItem.Manager.Name} {userItem.Manager.FamilyName}</p><span>{userItem.Manager.Position}</span></div>
               </div>)}
           </div>) : (<div>Немає інформації</div>)}
         {this.state.orgUser.length > 0 ? (
@@ -84,7 +82,7 @@ export default class VivaOrgstructure extends React.Component<IVivaOrgstructureP
                 <div className={styles.userPhoto}>
                   <img src={this.requestPhotoUrl(userItem.Login)} draggable="false" />
                 </div>
-                <div className={styles.userInfo}>{userItem.FullName}</div>
+                <div className={styles.userInfo}><p>{userItem.FullName}</p><span></span>{userItem.Position}</div>
               </div>)}
           </div>) : (<div>Немає інформації</div>)}
         {this.state.orgEmployees.length > 0 ? (
@@ -94,10 +92,61 @@ export default class VivaOrgstructure extends React.Component<IVivaOrgstructureP
                 <div className={styles.userPhoto}>
                   <img src={this.requestPhotoUrl(employeeItem.AccountName)} id={employeeItem.AccountName} draggable="false" />
                 </div>
-                <div className={styles.userInfo} id={employeeItem.AccountName}>{employeeItem.FullName}</div>
+                <div className={styles.userInfo} id={employeeItem.AccountName}><p>{employeeItem.FullName}</p><span></span>{employeeItem.Position}</div>
               </div>)}
           </div>) : (<div>Немає інформації</div>)}
-      </div>
+        <div className={classNames(styles.ms_PersonaCard)}>
+          <div className={classNames(styles.ms_PersonaCard_persona)}>
+            <div className={classNames(styles.ms_Persona, styles.ms_Persona__lg)}>
+              <div className={classNames(styles.ms_Persona_imageArea)}>
+                <div className={classNames(styles.ms_Persona_initials, styles.ms_Persona_initials__blue)}>AL</div>
+                <img className={classNames(styles.ms_Persona_image)} src={this.requestPhotoUrl(this.state.loginName)} />
+              </div>
+              <div className={classNames(styles.ms_Persona_details)}>
+                <div className={classNames(styles.ms_Persona_primaryText)}>Панюта Олександр Олександрович</div>
+                <div className={classNames(styles.ms_Persona_secondaryText)}>Розробник ПЗ на базі SharePiont</div>
+              </div>
+            </div>
+          </div>
+          <ul className={classNames(styles.ms_PersonaCard_actions)}>
+            <li data-action-id="chat" className={classNames(styles.ms_PersonaCard_action)}>
+              <i className="ms-Icon ms-Icon--Chat"></i>
+            </li>
+            <li data-action-id="phone" className={classNames(styles.ms_PersonaCard_action, styles.is_active)} >
+              <i className="ms-Icon ms-Icon--Phone"></i>
+            </li>
+            <li data-action-id="video" className={classNames(styles.ms_PersonaCard_action)}>
+              <i className="ms-Icon ms-Icon--Video"></i>
+            </li>
+            <li data-action-id="mail" className={classNames(styles.ms_PersonaCard_action)} >
+              <i className="ms-Icon ms-Icon--Mail"></i>
+            </li>
+          </ul>
+          <div className={classNames(styles.ms_PersonaCard_actionDetailBox)}>
+            <div data-detail-id="mail" className={classNames(styles.ms_PersonaCard_details)}>
+              <div className={classNames(styles.ms_PersonaCard_detailLine)}><span className={classNames(styles.ms_PersonaCard_detailLabel)}>Персональний E-mail: </span>
+                <a className={classNames(styles.ms_Link)} href="mailto: alton.lafferty@outlook.com">alton.lafferty@outlook.com</a>
+              </div>
+              <div className={classNames(styles.ms_PersonaCard_detailLine)}><span className={classNames(styles.ms_PersonaCard_detailLabel)}>Робочий E-mail: </span>
+                <a className={classNames(styles.ms_Link)} href="mailto: alton.lafferty@outlook.com">altonlafferty@contoso.com</a>
+              </div>
+            </div>
+            <div data-detail-id="phone" className={classNames(styles.ms_PersonaCard_details)}>
+              <div className={classNames(styles.ms_PersonaCard_detailLine)}>
+                <span className={classNames(styles.ms_PersonaCard_detailLabel)}>Додаткова інформація</span>
+              </div>
+              <div className={classNames(styles.ms_PersonaCard_detailLine)}>
+                <span className={classNames(styles.ms_PersonaCard_detailLabel)}>Персональний телефон:</span>  555.206.2443
+              </div>
+              <div className={classNames(styles.ms_PersonaCard_detailLine)}>
+                <span className={classNames(styles.ms_PersonaCard_detailLabel)}>Робочий телефон:</span>  555.929.8240
+              </div>
+            </div>
+          </div>
+        </div>
+        AL
+
+      </div >
     );
   }
 }
